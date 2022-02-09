5bbc7f654957734dd2a0bfd5616611352bc3fe98
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-02-10 01:17:33 +0300

    project created

---
+++ personListApp/personListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personListApp
+//
+//  Created by Дмитрий Смирнов on 10.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
